954e3566d24e8d6bf859c2d667a5ddb8e3807d71
hoangnam-dot <namhdhxhd@gmail.com>
2026-09-23 16:57:34 +0700

    feat(TTKN-26): register RoleAuthorizationMiddleware in Program.cs pipeline

--- src/EventTicketBooking.Api/Program.cs
+++ src/EventTicketBooking.Api/Program.cs
@@ -69,6 +69,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+app.UseMiddleware<EventTicketBooking.Api.Middlewares.RoleAuthorizationMiddleware>();
 app.MapControllers();
 
 app.Run();
