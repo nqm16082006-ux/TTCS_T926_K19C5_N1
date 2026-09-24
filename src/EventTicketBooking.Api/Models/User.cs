66f73213038b157477a9b8c0817d874d72c0bcf9
dtc245200854-byte <dtc245200854@ictu.edu.vn>
2026-09-24 10:50:30 +0700

    chore(T-08): organize email confirmation projects

--- src/EventTicketBooking.Api/Models/User.cs
+++ src/EventTicketBooking.Api/Models/User.cs
@@ -8,8 +8,6 @@ namespace EventTicketBooking.Api.Models
     /// </summary>
     public class User
     {
-        public string? VerificationCode { get; set; }
-        public DateTime? VerificationCodeExpiresAt { get; set; }
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Username { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
@@ -18,7 +16,9 @@ namespace EventTicketBooking.Api.Models
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
         public bool IsActive { get; set; } = false;
-
+public string? VerificationCode { get; set; }
+        public DateTime? VerificationCodeExpiresAt { get; set; }
+        
         // Navigation property quan hệ N:N thông qua bảng trung gian UserRoles
         public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
     }
