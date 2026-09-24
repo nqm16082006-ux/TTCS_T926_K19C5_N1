791bcaf553fb9333ded9fe540643498c38cc8956
LE BINH NGUYEN <dtc245160049@ictu.edu.vn>
2026-09-25 00:16:41 +0700

    Revert "feat(T-27): implement background job to release expired seat holds id…"

--- src/EventTicketBooking.Api/Models/Seat.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EventTicketBooking.Api.Models;
-
-public class Seat
-{
-    public int Id { get; set; }
-    public int ShowtimeId { get; set; }
-    public string Status { get; set; } = "AVAILABLE"; // AVAILABLE, HELD, SOLD
-}
